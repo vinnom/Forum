a8051e7640d441b7f52c917574e709937ed597f5
vinnom <vinnom.2d@gmail.com>
2020-09-23 17:57:47 -0300

    delega ao Spring o suporte à paginação

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,9 +8,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,9 +42,7 @@ public class TopicosController {
 
 	@GetMapping
 	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
-			@RequestParam int page, @RequestParam int size, @RequestParam String sort) {
-		
-		Pageable paginacao = PageRequest.of(page, size, Direction.DESC, sort);
+			@PageableDefault(page = 0, size = 10, sort = "id") Pageable paginacao) {
 		
 		if (nomeCurso == null) {
 			return TopicoDTO.converte(topicosRepository.findAll(paginacao));
